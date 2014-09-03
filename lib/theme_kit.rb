6073922c6cf9c158d73436123eb02b58e495cdd3
Howard Wilson <howard@watsonbox.net>
2014-09-03 16:17:57 +0200

    Add Sinatra-based ThemeKit webapp for working with the theme offline

---
+++ lib/theme_kit.rb
@@ -0,0 +1,68 @@
+require_relative 'liquid/tags/include'
+require_relative 'liquid/tags/captcha'
+require_relative 'liquid/tags/contact_form'
+require_relative 'liquid/tags/paginate'
+require_relative 'liquid/filters'
+
+Liquid::Tags::Include::INCLUDES_DIR = 'snippets'
+Liquid::Template.file_system = Liquid::LocalFileSystem.new(File.join(Dir.pwd, 'snippets'))
+
+class ThemeKit
+  PATHS = {
+    configs: 'configs',
+    layouts: 'layouts',
+    templates: 'templates',
+    stylesheets: 'stylesheets'
+  }
+
+  class << self
+    # Load offline store data from store.json
+    def load_store
+      @store = Hashie::Mash.new(JSON.parse(File.read('store.json')))
+    end
+
+    # Load default values from theme configuration file
+    def load_config
+      settings = JSON.parse(File.read(File.join(PATHS[:configs], 'default.json')))['settings']
+      config = {}
+
+      settings.each do |k, v|
+        config[k] = v['default'] if v['default']
+      end
+
+      @config = config
+    end
+
+    def load
+      load_store
+      load_config
+    end
+
+    def store
+      @store
+    end
+
+    def config
+      @config
+    end
+
+    def render(file, liquid_assigns = nil)
+      assigns = { 'config' => ThemeKit.config, 'store' => ThemeKit.store }
+
+      if liquid_assigns
+        liquid_assigns.each_key do |k|
+          unless liquid_assigns[k].is_a?(Hashie::Mash)
+            liquid_assigns[k] = Hashie::Mash.new(liquid_assigns[k])
+          end
+        end
+
+        assigns.merge!(liquid_assigns.stringify_keys)
+      end
+
+      layout = Liquid::Template.parse(File.read(File.join(PATHS[:layouts], 'default.html')))
+      template = Liquid::Template.parse(File.read(File.join(PATHS[:templates], file)))
+
+      layout.render(assigns.merge('content' => template.render(assigns)))
+    end
+  end
+end
---
+++ themekit.rb
@@ -0,0 +1,49 @@
+require 'sinatra'
+require 'liquid'
+require 'active_support/core_ext/string'
+require 'json'
+require 'hashie'
+
+require_relative 'lib/theme_kit'
+
+before do
+  ThemeKit.load
+end
+
+get '/' do
+  ThemeKit.render 'index.html'
+end
+
+get '/news' do
+  ThemeKit.render 'news.html', news: ThemeKit.store.news
+end
+
+get '/store' do
+  ThemeKit.render 'category.html', category: { name: 'All', products: ThemeKit.store.products }
+end
+
+get '/products/:slug' do
+  ThemeKit.render 'product.html', product: ThemeKit.store.products.find { |p| p.url == request.path_info }
+end
+
+get '/artists' do
+  ThemeKit.render 'roster.html', roster: ThemeKit.store.roster
+end
+
+get '/artists/:artist' do
+  item = ThemeKit.store.roster.items.find { |i| i.url == request.path_info }
+  item.products = ThemeKit.store.products
+
+  ThemeKit.render 'roster-item.html', item: item
+end
+
+get '/contact' do
+  ThemeKit.render 'contact.html'
+end
+
+get '/stylesheets/:file' do
+  content_type :css
+
+  css = File.read(File.join(ThemeKit::PATHS[:stylesheets], params[:file]))
+  Liquid::Template.parse(css).render('config' => ThemeKit.config)
+end
